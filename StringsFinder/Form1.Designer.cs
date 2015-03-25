5f9be9c5ccd8d1566b8e75fc601476300b7f1c28
simsilver <yihuanlingjian@gmail.com>
2015-03-25 20:26:00 +0800

    add weak win strings

---
+++ StringsFinder/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace StringsFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.matchListView = new System.Windows.Forms.ListView();
+            this.matchTextBox = new System.Windows.Forms.TextBox();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.matchTextLabel = new System.Windows.Forms.Label();
+            this.tipLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // matchListView
+            // 
+            this.matchListView.Location = new System.Drawing.Point(12, 12);
+            this.matchListView.Name = "matchListView";
+            this.matchListView.Size = new System.Drawing.Size(195, 238);
+            this.matchListView.TabIndex = 0;
+            this.matchListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // matchTextBox
+            // 
+            this.matchTextBox.Location = new System.Drawing.Point(255, 44);
+            this.matchTextBox.MaximumSize = new System.Drawing.Size(300, 4);
+            this.matchTextBox.Name = "matchTextBox";
+            this.matchTextBox.Size = new System.Drawing.Size(133, 21);
+            this.matchTextBox.TabIndex = 1;
+            this.matchTextBox.Text = "Model__Bone";
+            this.matchTextBox.WordWrap = false;
+            this.matchTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(213, 12);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(59, 12);
+            this.fileNameLabel.TabIndex = 2;
+            this.fileNameLabel.Text = "FileName:";
+            // 
+            // matchTextLabel
+            // 
+            this.matchTextLabel.AutoSize = true;
+            this.matchTextLabel.Location = new System.Drawing.Point(213, 47);
+            this.matchTextLabel.Name = "matchTextLabel";
+            this.matchTextLabel.Size = new System.Drawing.Size(41, 12);
+            this.matchTextLabel.TabIndex = 3;
+            this.matchTextLabel.Text = "Match:";
+            // 
+            // tipLabel
+            // 
+            this.tipLabel.AutoSize = true;
+            this.tipLabel.Location = new System.Drawing.Point(213, 82);
+            this.tipLabel.Name = "tipLabel";
+            this.tipLabel.Size = new System.Drawing.Size(131, 12);
+            this.tipLabel.TabIndex = 4;
+            this.tipLabel.Text = "Drag && Drop File Here";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 262);
+            this.Controls.Add(this.tipLabel);
+            this.Controls.Add(this.matchTextLabel);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.matchTextBox);
+            this.Controls.Add(this.matchListView);
+            this.Name = "Form1";
+            this.Text = "Strings";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView matchListView;
+        private System.Windows.Forms.TextBox matchTextBox;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Label matchTextLabel;
+        private System.Windows.Forms.Label tipLabel;
+    }
+}
+
---
+++ StringsFinder/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace StringsFinder
+{
+    public partial class Form1 : Form
+    {
+        private String mFileName = "";
+        private ArrayList mAllString = new ArrayList();
+        private int mMinLength = 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else e.Effect = DragDropEffects.None;
+
+            mFileName = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            int pos = mFileName.LastIndexOf('\\');
+            if (pos < 0)
+            {
+                pos = 0;
+            }
+            fileNameLabel.Text = "FileName: " + mFileName.Substring(pos + 1);
+        }
+
+        private void Form1_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            mFileName = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            ProceeFile(mFileName);
+            ProcessStrings();
+        }
+
+        private void ProcessStrings()
+        {
+            String matchText = matchTextBox.Text;
+            if (matchText.Length < mMinLength)
+            {
+                return;
+            }
+            String nowText;
+            int size = mAllString.Count;
+            int idx = 0;
+            matchListView.Items.Clear();
+            matchListView.View = View.List;
+            for (int i = 0; i < size; i++)
+            {
+                nowText = (String)mAllString[i];
+                idx = nowText.IndexOf(matchText);
+                if (idx >= 0)
+                {
+                    ListViewItem item = new ListViewItem();
+                    item.Text = nowText;
+                    matchListView.Items.Add(item);
+                }
+            }
+        }
+
+        private void ProceeFile(String fileName)
+        {
+            mAllString.Clear();
+            FileStream aFile = new FileStream(fileName, FileMode.Open);
+            int c = 0;
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                while ((c = aFile.ReadByte()) >= 0)
+                {
+                    if (c > 31 && c < 127)
+                    {
+                        sb.Append((char)c);
+                    }
+                    else if (sb.Length > 3)
+                    {
+                        mAllString.Add(sb.ToString());
+                        sb = new StringBuilder();
+                    }
+                }
+                if (sb.Length > 0)
+                {
+                    mAllString.Add(sb.ToString());
+                    sb = new StringBuilder();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            aFile.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (matchTextBox.Text.Length < mMinLength)
+            {
+                tipLabel.Text = "Match Length shoould > " + mMinLength;
+            }
+            else
+            {
+                tipLabel.Text = "Drag && Drop File Here.";
+                ProcessStrings();
+            }
+        }
+
+    }
+}
